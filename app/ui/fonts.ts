2b208534a009569b8123510f496108265cde80cc
Filipe Klinger <filipe@tg4.com.br>
2023-12-11 16:45:50 -0300

    Add inter font and import it in layout.tsx

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+//https://fonts.google.com/
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight: "400", subsets: ['latin'] });

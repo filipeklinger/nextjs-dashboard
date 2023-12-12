53a3ba2ff901437651eb2a98c79fc5023e342125
Filipe Klinger <filipe@tg4.com.br>
2023-12-12 17:57:34 -0300

    Add invoice edit page and form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,6 +38,30 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-    revalidatePath('/dashboard/invoices');
+    revalidatePath('/dashboard/invoices');//clear frontend cache
     redirect('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }

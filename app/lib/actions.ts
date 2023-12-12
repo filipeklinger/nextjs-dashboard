7d11a917846d6e92e4bf09c743250ea05accd649
Filipe Klinger <filipe@tg4.com.br>
2023-12-12 18:22:59 -0300

    Add invoice not found page and error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,68 +5,89 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    /**
-     * Tip: If you're working with forms that have many fields, you may want to consider using the entries()
-     * Method with JavaScript's Object.fromEntries(). 
-     * For example:
-     * const rawFormData = Object.fromEntries(formData.entries())
-     */
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
+  /**
+   * Tip: If you're working with forms that have many fields, you may want to consider using the entries()
+   * Method with JavaScript's Object.fromEntries(). 
+   * For example:
+   * const rawFormData = Object.fromEntries(formData.entries())
+   */
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
 
-    /**
-     * It's usually good practice to store monetary values in cents in your database to eliminate 
-     * JavaScript floating-point errors and ensure greater accuracy.
-     */
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-
+  /**
+   * It's usually good practice to store monetary values in cents in your database to eliminate 
+   * JavaScript floating-point errors and ensure greater accuracy.
+   */
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  try {
     await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-    revalidatePath('/dashboard/invoices');//clear frontend cache
-    redirect('/dashboard/invoices');
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+  } catch (error) {
+    console.log(error);
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
+
+  revalidatePath('/dashboard/invoices');//clear frontend cache
+  redirect('/dashboard/invoices');
+
 }
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
- 
+
 // ...
- 
+
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   const amountInCents = amount * 100;
- 
-  await sql`
+
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    };
+  }
+
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
 }
